ffac14afde40bb1f3df25c5e513b0d27ead6a90f
elvira <aelvira312@gmail.com>
2019-05-08 18:05:05 +0600

    add ExchangeFor enum

---
+++ src/main/java/com/example/ExchangeBook/enums/ExchangeFor.java
@@ -0,0 +1,10 @@
+package com.example.ExchangeBook.enums;
+
+public enum ExchangeFor {//обмен на что-нибудь
+    MONEY,//деньги
+    BOOK,//книга
+    OTHER;//что-нибудь другое
+
+    ExchangeFor() {
+    }
+}
