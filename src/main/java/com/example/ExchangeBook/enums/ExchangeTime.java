a000965dcac0ab61b4622d017b2c65b7a40c6d12
AseliaA <aselia.azimkanova@gmail.com>
2019-05-09 13:49:10 +0600

    Made a ExchangeTime Enum   + Added field "Forever"   + Added field "ForOneWeek"   + Added field "ForTwoWeek"   + Added field "ForThreeWeek"   + Added field "ForOneMonth"

---
+++ src/main/java/com/example/ExchangeBook/enums/ExchangeTime.java
@@ -0,0 +1,11 @@
+package com.example.ExchangeBook.enums;
+
+public enum ExchangeTime {
+    Forever,
+    ForOneWeek,
+    ForTwoWeek,
+    ForThreeWeek,
+    ForOneMonth;
+
+    ExchangeTime (){}
+}
