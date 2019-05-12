34b34499ecdaa0600ff363cd2bf99a178fb4e900
elvira <aelvira312@gmail.com>
2019-05-12 15:46:11 +0600

    add User (model)

--- src/main/java/com/example/ExchangeBook/model/User.java
+++ src/main/java/com/example/ExchangeBook/model/User.java
@@ -1,9 +1,42 @@
 package com.example.ExchangeBook.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import lombok.Data;
 
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
 @Entity
-@Table(name = "user_5")
+@Table(name = "book_user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    @Column(name = "name",nullable = false)
+    private String name;
+    @Column(name = "login",nullable = false,unique = true)
+    private String login;
+    @Column(name = "password",nullable = false,unique = true)
+    private String password;
+    @Column(name = "is_active",nullable = false)
+    private int isActive;
+    @Column(name = "phone_number",nullable = false)
+    private String phoneNumber;
+    @OneToMany
+    @JoinColumn(name = "book_id")
+    private List<Book>book;
+    @Column(name = "raiting")
+    private int raitingOfGenerosity;
+
+    public User() {
+    }
+
+    public User(String name, String login, String password, int isActive, String phoneNumber) {
+        this.name = name;
+        this.login = login;
+        this.password = password;
+        this.isActive = isActive;
+        this.phoneNumber = phoneNumber;
+    }
 }
