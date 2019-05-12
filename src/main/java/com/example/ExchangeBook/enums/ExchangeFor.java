ae6f48e4575b67249a8c161cbcecd88e8d397a02
elvira <aelvira312@gmail.com>
2019-05-12 11:09:29 +0600

    add JUST_GIVE in ExchangeFor change GIVVE -> GIVE

--- src/main/java/com/example/ExchangeBook/enums/ExchangeFor.java
+++ src/main/java/com/example/ExchangeBook/enums/ExchangeFor.java
@@ -3,7 +3,7 @@ package com.example.ExchangeBook.enums;
 public enum ExchangeFor {//обмен на что-нибудь
     MONEY,//деньги
     BOOK, //книга
-    JUST_GIVVE,//отдам просто так
+    JUST_GIVE,//отдам просто так
     OTHER;//что-нибудь другое
 
     ExchangeFor() {
